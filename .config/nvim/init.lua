5d5c28c4b9b35b671ffd9c4b88994159ee1aacbe
krunchington <krunchington@gmail.com>
2024-09-22 21:05:04 -0700

    fix whitespacing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,11 +20,11 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.opt.signcolumn = 'yes'
