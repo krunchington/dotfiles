9ff2f4d774d3203772220aa937764fb925151083
krunchington <krunchington@gmail.com>
2024-09-27 21:37:06 -0700

    Add trouble, some refactor

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,25 +6,28 @@ return {
             require('lspconfig').rust_analyzer.setup({
                 settings = {
                     ["rust-analyzer"] = {
-                        check = {
-                            command = "clippy"
-                        }
+                        -- check = {
+                        --     overrideCommand = {"cargo","clippy","--no-deps","--message-format=json"}
+                        -- }
+                        checkOnSave = { command = "clippy" }
+
                     }
                 }
             })
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
                 callback = function(event)
+                    local telescope = require('telescope.builtin')
                     local map = function(keys, func, desc, mode)
                         mode = mode or 'n'
                         vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
                     end
-                    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-                    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-                    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-                    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-                    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                    map('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+                    map('gr', telescope.lsp_references, '[G]oto [R]eferences')
+                    map('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+                    map('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
+                    map('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+                    map('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
                     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                     map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
                     map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
