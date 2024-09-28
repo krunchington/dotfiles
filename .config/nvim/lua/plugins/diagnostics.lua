f7657111f02f887e7edbd725eedc6801416840fd
krunchington <krunchington@gmail.com>
2024-09-28 08:49:19 -0700

    Add statusline, fidget

--- .config/nvim/lua/plugins/diagnostics.lua
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -1,4 +1,19 @@
 return {
+    { 'j-hui/fidget.nvim', opts = {} },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 
+            'nvim-tree/nvim-web-devicons',
+            -- 'nvim-lua/lsp-status.nvim'
+        },
+        config = function()
+            require("lualine").setup({
+            --     sections = {
+            --         lualine_y = {"require'lsp-status'.status()"}
+            --     }
+            })
+        end
+    },
     {
         "folke/trouble.nvim",
         opts = {}, -- for default options, refer to the configuration section for custom setup.
