ceaa5891f501d8875e90713803305d785841377f
krunchington <krunchington@gmail.com>
2025-03-15 14:13:05 -0700

    Add gitsigns mappings for hunk management

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -107,8 +107,28 @@ api.gitsigns = function(gitsigns, buffer)
         opts.buffer = buffer
         vim.keymap.set(mode, l, r, opts)
     end
+    map('n', ']c', function()
+        if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+        else
+            gitsigns.nav_hunk 'next'
+        end
+    end, { desc = 'Jump to next git [c]hange' })
+
+    map('n', '[c', function()
+        if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+        else
+            gitsigns.nav_hunk 'prev'
+        end
+    end, { desc = 'Jump to previous git [c]hange' })
+    map('v', '<leader>hr', function()
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end, { desc = 'git [r]eset hunk' })
+    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
     map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
     map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+    map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
 end
 
 api.telescope = function(telescope)
